7cb5da057b06e53a173d15dbd604cd090234f4e2
白咕咕不咕咕 <2052006294@qq.com>
2025-08-27 19:41:06 +0800

    second

--- H7_UART/Middleware/Inc/pid.h
+++ H7_UART/Middleware/Inc/pid.h
@@ -3,5 +3,29 @@
 
 #include "encoder.h"
 #include "motor.h"
+#include "main.h"
+
+
+
+typedef struct {
+    float kp;
+    float ki;
+    float kd;
+
+    float target;
+    float actual;
+    float out;
+
+    float Error0;
+	float Error1;
+	float ErrorInt;
+
+    float outMax;
+	float outMin;
+}pid_set;
+
+pid_set pid1;
+
+void pid_update(pid_set* p);
 
 #endif
